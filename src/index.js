89eb3d3522186f47fbedc51021e00c793ce440b5
mishaplx <olegorlov100oleg@gmail.com>
2021-12-12 21:52:27 +0300

    fix: add script

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 //import './css/style.scss';
-
-
+import slider from './slider.js'
+slider();
 const formBell = document.querySelector('.form-bell');
 formBell.addEventListener('click',()=>{
   formBell.classList.toggle('true')
